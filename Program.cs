56130191b7e5f1c1a847097f1ea25247136ccc3e
sanwar786 <95489644+sanwar786@users.noreply.github.com>
2022-01-31 21:00:44 +0530

    Commit Day4Assingments

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using PracticeProblemDay4.EmpWage;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeProblemDay4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //EmpPresentUC1.FindEmpPresent();
+            //EmpWageUC2.FindEmpWage();
+            //AddPartTimeWageUC3.FindPartTimeWage();
+            //EmpwageSwitchUC4.FindEmpWageSwitch();
+            //EmpWageMonthUC5.FindEmpWageForMonth();
+            //WageNumDayHourUC6.FindWageofWorkingDays();
+            Console.ReadLine();
+        }
+    }
+}
